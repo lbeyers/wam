408510741b7798dc09668584c58521fecd65a46e
Louise Beyers <louise@beyers.co.za>
2015-04-12 09:51:08 +0200

    Add timestamp to saved pages.

--- fetchweather.sh
+++ fetchweather.sh
@@ -1,3 +1,2 @@
-wget http://m.weather.com/weather/tomorrow/SFXX0444 -O weather_com.html
-wget http://www.accuweather.com/en/za/stellenbosch/301201/weather-forecast/301201 -O accuweather.html
-
+wget http://m.weather.com/weather/tomorrow/SFXX0444 -O weather_com_$(date +%Y%m%dT%H%M%S).html
+wget http://www.accuweather.com/en/za/stellenbosch/301201/weather-forecast/301201 -O accuweather_$(date +%Y%m%dT%H%M%S).html
