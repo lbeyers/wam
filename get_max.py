49c26b2f5d1aebba35030a7e43aade8a36d2f8fc
Louise Beyers <louise@beyers.co.za>
2015-04-27 11:35:59 +0200

    Got temperature with pyquery

--- get_max.py
+++ get_max.py
@@ -1,9 +1,17 @@
 #site = open("fetched_data/accuweather_
 from os import listdir
+from pyquery import PyQuery as pq
 filenames = listdir("fetched_data")
 filenames = [f for f in filenames if "accuweather_" in f and "T123" in f]
-print filenames
+#print filenames
 for filename in filenames: 
-    f = open("fetched_data/"+filename, "r")
-    print filename
-    print "fetched_data/"+filename
+    #print filename
+    #print "fetched_data/"+filename
+    with open("fetched_data/"+filename, "r") as f:
+        print filename
+        doc = pq(f.read())
+        #import pdb; pdb.set_trace()
+        temp = doc("#feed-sml-3 .city-fcast-text .temp")
+        temp = int(temp.text().split()[0])
+        print temp
+        #print f.read()
